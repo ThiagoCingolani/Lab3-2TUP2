52f62e048c8dcb06183cf01d2ff59d2c20f1ea76
ThiagoCingolani <thiagocingolani25@gmail.com>
2024-05-06 20:33:33 +0200

    practice_3

--- lab3practice/src/App.jsx
+++ lab3practice/src/App.jsx
@@ -1,29 +1,13 @@
 import './App.css'
-import Table from './practice_2/Table.jsx';
-
-const netIncomes =
-  [{ brand: "´McDonalds´", income: 1291283 },
-  { brand: "Burger King", income: 1927361 },
-  { brand: "‘KFC’", income: 1098463 }];
-
-const averageIncomes = netIncomes.reduce((acc, el) => acc + el.income / netIncomes.length, 0);
+import LoginForm from './practice_3/Login'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 
 function App() {
 
 return (
   <div>
-    <tr>
-      <th>Marca</th>
-      <th>Ingreso Neto</th>
-    </tr>
-    {netIncomes.map((netIncome, i) => (
-      <Table
-        key={i}
-        brand={netIncome.brand}
-        income={netIncome.income}
-      />
-    ))}
-    <p>Promedio del Ingreso Neto: ${averageIncomes.toFixed(2)}</p>
+    <LoginForm/>
   </div>
 )
 }
